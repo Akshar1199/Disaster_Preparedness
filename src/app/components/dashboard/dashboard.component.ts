4aa7d526e44d9384755ed2d130d5f5a2717c78aa
Akshar-Parekh-Inventyv <akshar.parekh.inventyv@gmail.com>
2025-02-16 18:17:49 +0530

    ngRX in dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,6 +11,10 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { Store } from '@ngrx/store';
+import * as DisasterActions from '../../store/actions/disaster.action';
+import { Observable, Subscription } from 'rxjs';
+import { selectDisasters } from '../../store/selectors/disaster.selector';
 
 @Component({
   selector: 'app-dashboard',
@@ -25,37 +29,49 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatDividerModule,
     MatInputModule,
     FormsModule,
-    MatSidenavModule
-],
+    MatSidenavModule,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  // disasters: any[] = [];
+  // filteredDisasters: any[] = [];
+  disasters$?: Observable<any[]>;
+  filteredDisasters$?: Observable<any[]>;
   disasters: any[] = [];
-  filteredDisasters: any[] = [];
   searchQuery: string = '';
-  sidebarOpen = true;
+
+  private bucketSubscription!: Subscription;
 
   // @ViewChild(NavBarComponent) navBar!: NavBarComponent;
 
-  constructor(private disasterServ: DisasterService, private router: Router) {}
+  constructor(
+    private disasterServ: DisasterService,
+    private store: Store<{disasters:any[]}>,
+    private router: Router
+  ) {}
+
 
   ngOnInit(): void {
     this.disasterServ.getData().subscribe((res) => {
-      this.disasters = res[0].disasters;
-      this.filteredDisasters = this.disasters;
+      const disasters = Array.isArray(res[0].disasters)
+        ? res[0].disasters
+        : Object.values(res[0].disasters);
+      this.store.dispatch(DisasterActions.loadDisastersSuccess({ disasters }));
+
+      this.filteredDisasters$ = this.store.select(selectDisasters);
     });
   }
 
-  onSearch(field:string) {
-    this.searchQuery = field;
-    const query = this.searchQuery.toLowerCase();
-    this.filteredDisasters = this.disasters.filter(disaster =>
-      disaster.category.toLowerCase().includes(query)
-    );
+  // onSearch(field:string) {
+  //   this.searchQuery = field;
+  //   const query = this.searchQuery.toLowerCase();
+  //   this.filteredDisasters = this.disasters.filter(disaster =>
+  //     disaster.category.toLowerCase().includes(query)
+  //   );
 
-  }
+  // }
 
   selectedDisaster(disaster: string) {
     this.router.navigate(['disaster/', disaster]);
@@ -65,11 +81,9 @@ export class DashboardComponent {
     this.router.navigate(['/login']);
   }
 
-  goToProfile(){
+  goToProfile() {
     this.router.navigate(['/profile']);
   }
-
-
 
   logout() {}
 }
